b8df50eb9cead0fd973d47f2623f0e7354a12e37
Bartek Krzyzak <bartekkrzyzak@icloud.com>
2025-04-03 08:50:49 +0200

    Corrected

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,4 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
 
@@ -12,7 +11,6 @@ class Question(models.Model):
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
